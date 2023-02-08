b66c700732ceab038fab113cf3e5d95df58791b8
SERGEI ARYTUYAN <sergei995007@mail.ru>
2023-02-08 21:31:17 +0300

    Find minumum Works!

--- ArrayProject.cpp
+++ ArrayProject.cpp
@@ -2,12 +2,30 @@
 
 int main()
 {   
-	const int SIZE=10;
-	int mas[SIZE];
-	int i;
-	for (i = 0; i < 10; i++) {
-		mas[i]=i+1;
-	std::cout<<mas[i] <<' ';
+	const int SIZE=20;
+	const int MAX_RAND_VALUE = 1000;
+	const int MIN = MAX_RAND_VALUE+1;
+	unsigned short mas[SIZE];
+	short i;
+	for (i = 0; i < SIZE; i++) {
+		mas[i] = std::rand() % MAX_RAND_VALUE;
+	
 	}
-}
+	
+	
+	//вывод массива на консоль
+	for (i = 0; i < SIZE; i++) {
+		std::cout << mas[i] << ' ';
+
+	}
+	//Поиск минимумального значения в массива
+	for (i=0;i<SIZE;i++) {
+		if (mas[i] < MIN) {
+			MIN < mas[i];
+		}
+		
+		std::cout << std::endl << "Minimum is:" << MIN;
+	}
+} 
+
 
